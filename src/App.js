ac696a6d43461f13136e7c12653572ad5eb18c40
Musfiqur Rahman Zihad <musfiqurahmanzihad@gmail.com>
2024-03-10 22:28:33 +0600

    functional remove button added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import 'remixicon/fonts/remixicon.css';
+let todoList = [
+  {
+    id: crypto.randomUUID(),
+    todo: "Finish homework"
+  },
+  {
+    id: crypto.randomUUID(),
+    todo: "Buy groceries"
+  },
+  {
+    id: crypto.randomUUID(),
+    todo: "Call mom"
+  },
+];
 
 function App() {
+  const [todos, setTodos] = useState(todoList);
+  // console.log(todos);
+  const [newTodo, setNewTodo] = useState('');
+  const removeTodo = (index) => {
+    const newTodos = todos;
+    const filteredTodos = newTodos.filter((todo, idx) => idx !== index)
+    setTodos(filteredTodos);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+      <div className="center-container">
+        <TodoInput todos={todos} setTodos={setTodos} newTodo={newTodo} setNewTodo={setNewTodo} />
+        <TodoList setTodos={setTodos} todos={todos} onRemove={removeTodo} />
+      </div>
     </div>
   );
+};
+
+function TodoInput({ newTodo, setNewTodo, setTodos }) {
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const id = crypto.randomUUID();
+    const todo = newTodo;
+
+    const newTodo1 = {
+      id,
+      todo
+    }
+    setTodos((todos) => [...todos, newTodo1]);
+    setNewTodo("");
+  }
+
+  return (
+    <form className="input-container" onSubmit={handleSubmit}>
+      <input value={newTodo} onChange={(e) => setNewTodo(e.target.value)}
+        type="text"
+        className="input-box-todo"
+        placeholder="Enter your todo"
+      />
+      <button className="add-btn">+</button>
+    </form>
+  );
+}
+
+function TodoList({ todos, onRemove }) {
+
+  return (
+    <ul>
+      {
+        todos.map((todo, index) => (
+          <li key={todo.id} className="list-item">
+            {todo.todo}
+            <span className='icons' onClick={() => onRemove(index)}>
+              <i className="ri-delete-bin-6-line"></i>
+            </span>
+          </li>))
+      }
+    </ul>
+  )
 }
 
 export default App;
